af7e549863ffebc20bd924fcd93291381e28ea34
Jorge <arm.gomez@outlook.com>
2019-08-26 14:40:59 -0500

    Update

---
+++ temp/script.js
@@ -0,0 +1,13 @@
+$('#vimeo').magnificPopup({
+	items: {
+		src: 'https://vimeo.com/148198462'
+	},
+	type: 'iframe'
+});
+
+$('#youtube').magnificPopup({
+	items: {
+		src: 'https://www.youtube.com/watch?v=124qhYkJG2w'
+	},
+	type: 'iframe'
+});
